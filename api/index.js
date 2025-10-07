b3e84b12566d5285c9b266661a163444d6095349
sebasperezcabanas <sebasperezcabanas@gmail.com>
2025-10-07 16:51:27 -0300

    test

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ mensaje: "Bienvenido a API SL IEA" });
+  res.json({ mensaje: "Bienvenido a API SL IEA - Prueba" });
 });
 
 app.listen(PORT, () => {
